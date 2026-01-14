b947160b84e8f2c84deee110a99980767af2ab29
xIrusux <christine.altmann@gmx.com>
2026-01-14 10:53:11 +0100

    set up studio fe

---
+++ src/Resources/public/studio/build/5c6456a3-c836-42b5-a462-06b1532484f8/exposeRemote.js
@@ -0,0 +1,7 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      window.pluginRemotes.pimcore_objectmerger_bundle = "http://localhost:3033/static/js/remoteEntry.js"
+    
