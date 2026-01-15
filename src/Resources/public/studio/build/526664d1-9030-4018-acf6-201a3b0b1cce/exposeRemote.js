0c5e6dd4673d5b198b20404146344e1927885e5b
xIrusux <42359615+xIrusux@users.noreply.github.com>
2026-01-15 09:14:16 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/526664d1-9030-4018-acf6-201a3b0b1cce/exposeRemote.js
+++
@@ -1,7 +0,0 @@
-
-      if (window.pluginRemotes === undefined) {
-        window.pluginRemotes = {}
-      }
-
-      window.pluginRemotes.pimcore_objectmerger_bundle = "http://localhost:3033/static/js/remoteEntry.js"
-    
