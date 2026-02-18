f4bf908c3dcb21873db363de55b7d58b83d6540b
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-02-18 18:42:29 +0100

    updated the rsbuild config

--- src/Resources/public/studio/build/7cd2589e-1c49-4de1-9674-b88c2473c540/exposeRemote.js
+++ src/Resources/public/studio/build/7cd2589e-1c49-4de1-9674-b88c2473c540/exposeRemote.js
@@ -3,5 +3,11 @@
         window.pluginRemotes = {}
       }
 
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
       window.pluginRemotes.pimcore_objectmerger_bundle = "http://localhost:3033/static/js/remoteEntry.js"
+
+      
     
