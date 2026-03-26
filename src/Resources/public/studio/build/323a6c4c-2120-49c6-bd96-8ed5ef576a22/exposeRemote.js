3e0d9ddcdd5b22833d05fb345145e4fb52a0958d
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-03-26 13:24:47 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/323a6c4c-2120-49c6-bd96-8ed5ef576a22/exposeRemote.js
+++ src/Resources/public/studio/build/323a6c4c-2120-49c6-bd96-8ed5ef576a22/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/2402d067-7e9d-4d26-a9ee-bbdb5994344a/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/323a6c4c-2120-49c6-bd96-8ed5ef576a22/static/js/remoteEntry.js"
 
       
     
