591873e5444bbdd2e94d9004fd78df267d3c9d5c
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-03-26 14:22:45 +0100

    Merge pull request #87 from pimcore/update-packages
    fix: migrate ESLint to v9 flat config and remove unused devDependencies

--- src/Resources/public/studio/build/2402d067-7e9d-4d26-a9ee-bbdb5994344a/exposeRemote.js
+++ src/Resources/public/studio/build/2402d067-7e9d-4d26-a9ee-bbdb5994344a/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/b4507af7-d80c-481f-8c77-4074f84681e6/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/2402d067-7e9d-4d26-a9ee-bbdb5994344a/static/js/remoteEntry.js"
 
       
     
