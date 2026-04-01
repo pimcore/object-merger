002cc2648eb1901722e138bc8995566e81b1a6f6
ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>
2026-04-01 12:10:48 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/cc04d719-59b7-4cfa-ad6a-3b7e12642e7b/exposeRemote.js
+++ src/Resources/public/studio/build/cc04d719-59b7-4cfa-ad6a-3b7e12642e7b/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/491e6beb-06d3-4ea5-a2b0-380494f03367/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/cc04d719-59b7-4cfa-ad6a-3b7e12642e7b/static/js/remoteEntry.js"
 
       
     
