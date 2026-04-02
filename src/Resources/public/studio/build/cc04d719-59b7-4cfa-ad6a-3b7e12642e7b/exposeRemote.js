931e9554b2870ef81f5c8810887c77cd5bc49cbf
Valeriia Maltseva <valeriia.maltseva@pimcore.com>
2026-04-02 12:21:09 +0200

    [Improvement]: Add predefined object integration to the Compare page form (#92)
    * added logic of handling predefined selectedMergerObjects
    
    * added the fetchFullPaths
    
    * Automatic frontend build
    
    * Automatic frontend build
    
    ---------
    
    Co-authored-by: ValeriaMaltseva <11871778+ValeriaMaltseva@users.noreply.github.com>

---
+++ src/Resources/public/studio/build/cc04d719-59b7-4cfa-ad6a-3b7e12642e7b/exposeRemote.js
@@ -0,0 +1,13 @@
+
+      if (window.pluginRemotes === undefined) {
+        window.pluginRemotes = {}
+      }
+
+      if (window.alternativePluginExportPaths === undefined) {
+        window.alternativePluginExportPaths = {}
+      }
+
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/cc04d719-59b7-4cfa-ad6a-3b7e12642e7b/static/js/remoteEntry.js"
+
+      
+    
