f11b4d292a80edae6d60892852131dd4ae17c88c
berfinyuksel <99557970+berfinyuksel@users.noreply.github.com>
2026-07-24 09:54:16 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/e18b8451-ebec-4d45-a28e-9522220d7bfb/exposeRemote.js
+++ src/Resources/public/studio/build/e18b8451-ebec-4d45-a28e-9522220d7bfb/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/e21bd3c8-2d1a-46a6-bff9-87a75488f260/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/e18b8451-ebec-4d45-a28e-9522220d7bfb/static/js/remoteEntry.js"
 
       
     
