599f45aae64edcc2dfed4d903363f2e72102a3f9
pimcore-deployments <71881008+pimcore-deployments@users.noreply.github.com>
2026-08-12 13:11:52 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/5606d623-afad-4278-8504-5fd8c2974169/exposeRemote.js
+++ src/Resources/public/studio/build/5606d623-afad-4278-8504-5fd8c2974169/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/a38af697-4d2a-40aa-b0d0-17e65dc71ed3/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/5606d623-afad-4278-8504-5fd8c2974169/static/js/remoteEntry.js"
 
       
     
