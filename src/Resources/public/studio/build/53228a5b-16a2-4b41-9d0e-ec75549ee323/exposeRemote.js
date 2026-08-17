9b50872a324308a5e51c091204780cb61cd5da49
xIrusux <42359615+xIrusux@users.noreply.github.com>
2026-08-17 15:39:16 +0000

    Automatic frontend build

--- src/Resources/public/studio/build/53228a5b-16a2-4b41-9d0e-ec75549ee323/exposeRemote.js
+++ src/Resources/public/studio/build/53228a5b-16a2-4b41-9d0e-ec75549ee323/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/e1addabf-fc40-4f8e-ab42-08bf131e777a/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_objectmerger_bundle = "/bundles/objectmerger/studio/build/53228a5b-16a2-4b41-9d0e-ec75549ee323/static/js/remoteEntry.js"
 
       
     
